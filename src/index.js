bc6fbf89d73b2bbc743cc7405eea3977e5ba7e92
wazerbwsed22 <Utsha.paul@stonybrook.edu>
2023-07-21 17:47:51 -0400

    yes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 import { HashRouter as Router } from "react-router-dom";
-
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Router>
-      <App />
-    </Router>
+    // <Router>
+      
+    //   <App />
+    // </Router>
+    <BrowserRouter basename="/swe-portfolio">
+    
+      <Routes>
+        <App/>
+        <Route path="/" /> 
+      </Routes>
+    </BrowserRouter>
+    
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
